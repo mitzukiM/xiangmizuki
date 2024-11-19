f23c2aa1e3b61fa6de24ae009bbfc5671fd8d93a
Margo <you@example.com>
2024-11-19 13:02:01 +0200

    modified file

---
+++ first_work_with_strings.py
@@ -0,0 +1,8 @@
+secondname = ' Це стрічка з лапками всередині: "Цитата" '
+print(secondname)
+print(id(secondname))
+thirdname = ' U+1F941'
+print(thirdname)
+print(id(thirdname))
+print('\U0001F941')
+print(chr(190))
