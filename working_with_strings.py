dd1d7f00db32e12c07a98af4c53e518229c4a932
Margo <you@example.com>
2024-11-23 22:18:13 +0200

    modified fiels

---
+++ working_with_strings.py
@@ -0,0 +1,36 @@
+
+
+#
+#
+#
+# firstname = "Alex"
+# # print(id(firstname))
+# lastname = "Obama"
+#
+# # firstname = 'Max'
+# # print(id(firstname))
+# fullname = firstname + " " + lastname
+# welcome_message = "Welcome, Mr." + " " + fullname
+# print(welcome_message)
+#
+# reverted_fullname = lastname + firstname
+#
+#
+#
+# print(fullname)
+# print(reverted_fullname)
+#
+# print(firstname, lastname, lastname, fullname)
+my_text = 'my best text'
+
+pass
+
+
+
+
+
+
+
+
+
+
