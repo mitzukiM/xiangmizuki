f042bb496c856663fad123f616db80dda73f3d5d
Margo <you@example.com>
2024-12-01 14:29:45 +0200

    more into about

---
+++ correcting_mistakes.py
@@ -0,0 +1,9 @@
+import info
+
+# print("\U0001F463")
+smile_footprint = "\U0001F463"
+
+user_name = "Max"
+user_age = "5"
+result = f" My name is  {user_name}  I am {user_age} years old {smile_footprint}"
+print(result)
---
+++ info.py
@@ -0,0 +1,1 @@
+INFO = "My name is {name}, I am {age} years old{picture}"
