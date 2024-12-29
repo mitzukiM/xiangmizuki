56d5235f08b3dbd6e1af1fd39996dc7d63ef606f
Margo <you@example.com>
2024-12-29 18:27:33 +0300

    more into about

--- web_homework_15.py
+++ web_homework_15.py
@@ -2,7 +2,6 @@ import requests
 
 link = 'https://dummyjson.com/users'
 
-
 params = {
     'limit': 3000,
     'skip': 0
@@ -13,21 +12,17 @@ response_json = response.json()
 
 users_link = response_json['users']
 
+who_lives_in_san_francisco = 0
+green_eyed_females_count = 0
 users_under_of_30 = 0
 for user in users_link:
     if user['age'] < 30:
         users_under_of_30 += 1
 
-green_eyed_females_count = 0
-
-for user in users_link:
     if user['gender'] == 'female' and user['eyeColor'] == 'Green':
         green_eyed_females_count += 1
 
-who_lives_in_San_Francisco = 0
-
-for user in users_link:
     if 'San Francisco' in user['address']['city']:
-        who_lives_in_San_Francisco += 1
+        who_lives_in_san_francisco += 1
 
 pass
