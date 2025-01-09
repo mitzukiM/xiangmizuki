40b9090bcda1ebcce5322b3a2ac3c06eec35e9bf
Margo <you@example.com>
2025-01-09 20:21:51 +0300

    more into about

--- practice_web_3.py
+++ practice_web_3.py
@@ -9,10 +9,11 @@ response = requests.get(url, params=params)
 response_json = response.json()
 
 people_in_space = response_json['people']
+human_in_iss = []
 
 for human in people_in_space:
     if human['craft'] == 'ISS':
         human_name = human['name']
-        print(human_name)
+        human_in_iss.append(human_name)
 
 pass
